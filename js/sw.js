1355d7c1abc2b48bd31c7d976d8606d085cc0472
G <regina.ruark@gmail.com>
2018-09-03 17:51:36 -0400

    Merge index and sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,31 +1,41 @@
-var cacheID = "mws-restaurant-001";
+const cacheName = 'v1';
 
-self.addEventListener("install", event => {
-		event.waitUntil {
-			caches.open(cacheID).then(cache => {
-				return cache
-				.addAll([
-					"/",
-					"/index.html",
-					"/restuarant.html",
-					"/css/styles.css",
-					"/data/restaurants.json",
-					"/js/"
-			    ])
-			}
-		}
-}
 
-//If page cannot be found
-self.addEventListener('fetch', function(event){
-	event.respondWith(
-		fetch(event.request).then(function(response) {
-			if (response.status == 404){
-				return new Response("Page not found");
-			}
-			 return response;
-		}).catch(function() {
-			return new Response("Request failed.");
-	})
-  );
-}
+self.addEventListener('install', event => {
+	console.log('Service Worker Installed');
+});
+
+//Call Activate Event
+self.addEventListener('activate', event => {
+	console.log('Service Worker activated');
+	//Remove unwanted caches
+    event.waitUntil(
+		 caches.keys().then(cacheNames => {
+			return Promise.all(
+			   cacheNames.map(cache => {
+				   if (cache !== cacheName) {
+					   console.log('Service Worker: clearing old cache');
+					   return caches.delete(cache);
+				   }
+		      })
+				);
+		  })
+     );
+});
+
+//Call Fetch Event
+self.addEventListener('fetch', event => {
+		console.log('Service Worker: fetching');
+		event.respondWith(
+		    fetch(event.request)
+			 .then(res => {
+				 const resClone = res.clone();
+				 caches
+				 .open(cacheName)
+				 .then(cache => {
+					 cache.put(event.request, resClone);
+					   });
+				   return res;
+				   }).catch(err => caches.match(event.request).then(res => res))
+		);
+});
